afcb5b407e9d9d2b96369c34ae01bb8236c19dcb
Lindsey <linds.white95@gmail.com>
2018-04-29 21:37:23 -0500

    Add updates

--- practicePackages.py
+++ practicePackages.py
@@ -1,4 +1,5 @@
-import math, sys, os, logging, datetime, csv, json, sqlite3
+import math, sys, os, logging, datetime, csv, json, sqlite3, itertools
+from functools import partial
 
 def doThings(num1, num2):
 	# find GCD with math package
@@ -27,21 +28,27 @@ def doThings(num1, num2):
 	with open('data.txt', 'r') as f:
 		d = json.load(f)
 		print(d)
-	return gcd
-	# sqlite3 practice
 	sqFile = 'text.sqlite'
 	conn = sqlite3.connect(sqFile)
 	c = conn.cursor()
-	c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-		.format(tn="Table", cn="Column", ct="INTEGER"))
-	c.execute("UPDATE {tn} SET {cn}='Prices' WHERE {idf}=123456"\
-		.format(tn="Table", idf="My Column", cn="Column"))
-	c.execute('CREATE INDEX {ix} on {tn}({cn})'\
-		.format(ix="thisIndex", tn ="Table", cn="Column"))
-	c.execute('DROP INDEX {ix}'.format(ix="thisIndex"))
-	conn.commit()
 	conn.close()
+	# iterate through all of these strings with chain in itertools
+	for i in itertools.chain("LEM", "ON", "ADE"):
+		print(i)
+	return gcd
+
+# practice with functools
+def power(base, exponent):
+	return base ** exponent
+
+def partialStuff():
+	assert square(2) == 4
+	assert cube(2) == 8
 
 def main():
 	print("GCD is:", doThings(85, 400))
+	square = partial(power, exponent=2)
+	cube = partial(power, exponent=3)
+	print(cube(2))
+
 main()
